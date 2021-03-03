8f8b195408955a9e702c1101a335ccaf10a9b8bc
Weber <weber12332111778@gmail.com>
2021-03-03 19:00:04 +0800

    試build

--- GameServer/src/com/HttpServer/Manager/GameRoomManager.java
+++ GameServer/src/com/HttpServer/Manager/GameRoomManager.java
@@ -1,5 +1,6 @@
 package com.HttpServer.Manager;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -31,12 +32,12 @@ public class GameRoomManager {
     public String[] CreateNewGameRoom(JSONObject jdata) {
         String[] Key = new String[2];
         try {
-            Key[0] = GetKey(jdata.getString("blueTeamName") + jdata.getString("redTeamName")
+            Key[0] = getKey(jdata.getString("blueTeamName") + jdata.getString("redTeamName")
                     + jdata.getString("gameName") + System.currentTimeMillis());
-            Key[1] = GetKey(System.currentTimeMillis() + "").substring(5, 12);
+            Key[1] = getKey(System.currentTimeMillis() + "").substring(5, 12);
         } catch (Exception e) {
         }
-        if (Key[0] != "") {
+        if (Key[0].equals("")) {
             // 這邊可以換成可擴充結構
             GameRoom newGameRoom = new LOLM3BanRoom(jdata, Key);
             synchronized (AllGame) {
@@ -73,14 +74,14 @@ public class GameRoomManager {
         }
     }
 
-    private String GetKey(String str) {
+    private String getKey(String str) {
         char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-            messageDigest.update(str.getBytes("UTF-8"));
+            messageDigest.update(str.getBytes(StandardCharsets.UTF_8));
             // Hash計算, 產生128位的長整數
             byte[] bytes = messageDigest.digest();
-            StringBuffer sb = new StringBuffer(bytes.length * 2);
+            StringBuilder sb = new StringBuilder(bytes.length * 2);
             for (Byte b : bytes) {
                 // 右移四位, 取字節中前四位轉換
                 sb.append(hexDigits[(b >> 4) & 0x0f]);
